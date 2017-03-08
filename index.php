15f291cafe4a62e1ef625d59411e064cd40e7ccf
Henrik <henrikmottus@gmail.com>
2017-03-08 14:00:06 +0200

    javascripti kell lisatud

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
+<script type="text/javascript">
+    //JS realtime clock woot
+    window.onload = function (){
+        (function (){
+            var date = new Date();
+            var time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+            document.getElementsByTagName('div')[0].innerHTML = "JS aeg: " + time;
+            window.setTimeout(arguments.callee, 1000);
+        })();
+    };
+
+</script>
+
 <?php
 
-echo "08.03.2017";
+$date = "08.03.2017";
+$number = 5;
 
+echo $date.", kolmas tund.<br>";
 
+function aeg(){
+    date_default_timezone_set("Europe/Tallinn");
+    $time = date("H:i:s");
+    echo $time."<br>";
+    if($time > "16:00"){
+        echo "Tund läbi, järgmine tund!";
+    }
+    else{
+        echo "Peab kannatama!";
+    }
+}
+echo "<div></div>";
+
+aeg();
 ?>
